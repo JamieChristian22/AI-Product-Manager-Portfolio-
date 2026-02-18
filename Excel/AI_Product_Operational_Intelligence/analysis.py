c73cfb2730c7b868ae28c56eee169ae58b3361e5
Jamie Christian II <jamiechristian557@gmail.com>
2026-02-18 12:37:13 -0500

    Delete Excel directory

--- Excel/AI_Product_Operational_Intelligence/analysis.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('dataset.csv')
-print('Conversion Rate:', df['conversion'].mean())
-print('Average Session:', df['session_length'].mean())
-print('Total Revenue:', df['revenue'].sum())
--- Excel/AI_Product_Operational_Intelligence/queries.sql
+++
@@ -1,8 +0,0 @@
-SELECT AVG(session_length) FROM dataset;
-
-SELECT COUNT(*) FROM dataset WHERE conversion = 1;
-
-SELECT SUM(revenue) FROM dataset;
-
-SELECT retention_flag, COUNT(*) FROM dataset GROUP BY retention_flag;
-
